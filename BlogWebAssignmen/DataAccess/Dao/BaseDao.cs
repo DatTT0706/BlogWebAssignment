644f320bda55f0a28b4c2e207ffb110f12f75fed
dngoc <Dung.CaoNgoc@ingenico.com>
2022-07-11 21:12:49 +0700

    Update for business Object and basedao

---
+++ BlogWebAssignmen/DataAccess/Dao/BaseDao.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.Dao
+{
+    public interface BaseDao<T>
+    {
+        public IEnumerable<T> GetAll();
+        public T GetById(int id);
+        public T GetByName(string name);
+        public void Update(T entity);
+        public void DeleteById(int id);
+        public void Save(T entity);
+    }
+}
---
+++ BlogWebAssignmen/DataAccess/Dao/TagDao.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DataAccess.Models;
+
+namespace DataAccess.Dao
+{
+    internal class TagDao : BaseDao<Tag>
+    {
+        public void DeleteById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Tag> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Tag GetById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Tag GetByName(string name)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Save(Tag entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(Tag entity)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
