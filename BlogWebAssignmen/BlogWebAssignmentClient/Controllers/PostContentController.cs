d67052b36beac4ec20ad0f6f2de10eadcc1f2d30
ChisKien <chiskien03214@gmail.com>
2022-07-23 00:21:50 +0700

    PostContentController.cs created

---
+++ BlogWebAssignmen/BlogWebAssignmentClient/Controllers/PostContentController.cs
@@ -0,0 +1,15 @@
+﻿using DataAccess.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BlogWebAssignmentClient.Controllers
+{
+    public class PostContentController : Controller
+    {
+        public User User { get; set; }
+
+        public IActionResult Index()
+        {
+            return View(User);
+        }
+    }
+}
---
+++ BlogWebAssignmen/BlogWebAssignmentClient/Controllers/RegisterController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BlogWebAssignmentClient.Controllers
+{
+    public class RegisterController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
