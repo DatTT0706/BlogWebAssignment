98bf87bf77499ce1cb5534cb24c0425e546594f4
ChisKien <chiskien03214@gmail.com>
2022-07-22 15:48:45 +0700

    display category on navbar

---
+++ BlogWebAssignmen/BlogWebAssignmentClient/wwwroot/js/fetchCategory.js
@@ -0,0 +1,41 @@
+﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
+// for details on configuring this project to bundle and minify static web assets.
+const apiCategory = "https://localhost:5001/api/Category";
+
+async function getData() {
+    await fetch(apiCategory, {
+        headers: {
+            "Content-Type": "application/json"
+        }, method: "get"
+    }).then(res => res.json())
+        .then(data => {
+            displayCategory(data);
+            displayCategoryNavbar(data);
+        })
+        .catch(e => console.error(e));
+}
+
+async function displayCategory(data = []) {
+    let tagListContainer = await document.querySelector(".tags-list");
+    await data.forEach((item) => {
+        let a = document.createElement("a");
+        a.href = "https://www.google.com";
+        a.className = "link-dark custom-tags";
+        a.innerHTML = `<span>${item["title"]}</span>`;
+        tagListContainer.appendChild(a);
+    })
+}
+
+async function displayCategoryNavbar(data = []) {
+    let ul = await document.getElementById("categoryList");
+    await data.forEach((item) => {
+        let li = document.createElement("li");
+        li.className = "nav-item";
+        li.innerHTML = `<a class="nav-link text-dark" href="http://localhost:8080">${item["title"]}</a>`;
+        ul.appendChild(li);
+    })
+}
+
+getData();
+
+// Write your JavaScript code.
