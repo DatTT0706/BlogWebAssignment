9a17a19344cf848ac8d4db69dfcdd346e89cc791
Nguyen The Hai <hainguyenn2401@gmail.com>
2022-07-23 03:12:39 +0700

    User api

--- BlogWebAssignmen/DataAccess/DTO/UserDTO.cs
+++ BlogWebAssignmen/DataAccess/DTO/UserDTO.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace DataAccess.DTO
 {
-    class UserDTO
+    public class UserDTO
     {
         public int Id { get; set; }
         public string FirstName { get; set; }
