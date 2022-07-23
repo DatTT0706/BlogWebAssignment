4f49656ba962e3b8c3da05a0d8497323bef02168
ChisKien <chiskien03214@gmail.com>
2022-07-23 12:48:37 +0700

    User dropdown

--- BlogWebAssignmen/DataAccess/Models/Post.cs
+++ BlogWebAssignmen/DataAccess/Models/Post.cs
@@ -1,7 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations;
-using System.ComponentModel.DataAnnotations.Schema;
 
 #nullable disable
 
@@ -11,30 +9,26 @@ namespace DataAccess.Models
     {
         public Post()
         {
+            PostCategories = new HashSet<PostCategory>();
             PostComments = new HashSet<PostComment>();
             PostMeta = new HashSet<PostMeta>();
         }
-        [Key, DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+
         public int Id { get; set; }
-        [Required]
-        public int AuthorId { get; set; }
+        public int? AuthorId { get; set; }
         public int? ParentId { get; set; }
-        [Required,StringLength(100)]
         public string Title { get; set; }
-        [Required, StringLength(150)]
         public string MetaTitle { get; set; }
-        [Required, StringLength(150)]
         public string Slug { get; set; }
-        [Required, StringLength(255)]
         public string Summary { get; set; }
         public int? Published { get; set; }
         public DateTime? CreatedAt { get; set; }
         public DateTime? UpdatedAt { get; set; }
         public DateTime? PublishedAt { get; set; }
-        [Required]
         public string Content { get; set; }
 
         public virtual User Author { get; set; }
+        public virtual ICollection<PostCategory> PostCategories { get; set; }
         public virtual ICollection<PostComment> PostComments { get; set; }
         public virtual ICollection<PostMeta> PostMeta { get; set; }
     }
