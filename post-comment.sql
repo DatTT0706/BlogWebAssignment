9007b8d183acb185c7da69318b584a662c5aa163
ChisKien <chiskien03214@gmail.com>
2022-07-23 23:28:20 +0700

    Fix script

--- post-comment.sql
+++ post-comment.sql
@@ -1,3 +1,4 @@
+use PRN231_Blog
 exec sp_rename 'post_comment.parentId', userId, 'COLUMN'
 go
 
@@ -7,11 +8,11 @@ go
 
 alter table post_comment
     add constraint post_comment_user_id_fk
-        foreign key (userId) references [user]
+        foreign key (userId) references [user](id)
 go
 
-UPDATE PRN231_Blog.dbo.post_comment  userId = 1, WHERE id = 1;
-UPDATE PRN231_Blog.dbo.post_comment  userId = 2, WHERE id = 2;
-UPDATE PRN231_Blog.dbo.post_comment  userId = 3, WHERE id = 3;
-UPDATE PRN231_Blog.dbo.post_comment  userId = 4, WHERE id = 4;
-UPDATE PRN231_Blog.dbo.post_comment  userId = 4, WHERE id = 5;
+UPDATE PRN231_Blog.dbo.post_comment set userId = 1 WHERE id = 1;
+UPDATE PRN231_Blog.dbo.post_comment set userId = 2 WHERE id = 2;
+UPDATE PRN231_Blog.dbo.post_comment set userId = 3 WHERE id = 3;
+UPDATE PRN231_Blog.dbo.post_comment set userId = 4 WHERE id = 4;
+UPDATE PRN231_Blog.dbo.post_comment set userId = 4 WHERE id = 5;
