2cd4a96a976c1c28f95b45ea54c88606b1b5961d
longnguyen13th1d.pou <longnguyen13th1d.pou@gmail.com>
2022-07-23 22:39:26 +0700

    blog admin

---
+++ BlogWebAssignmen/BlogWebAssignmentAdmin/Controllers/TagController.cs
@@ -0,0 +1,46 @@
+﻿using BlogWebAssignmentAdmin.Security;
+using DataAccess.DTO;
+using DataAccess.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Net.Http.Json;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace BlogWebAssignmentAdmin.Controllers
+{
+    public class TagController : Controller
+    {
+        public User Author { get; set; }
+        private readonly HttpClient _client;
+        private readonly IConfiguration _config;
+        private readonly ITokenService _tokenService;
+        private readonly string PostApi = "https://localhost:5001/api/Tag/";
+        public TagController(IConfiguration config, ITokenService tokenService)
+        {
+            _client = new HttpClient();
+            var contentType = new MediaTypeWithQualityHeaderValue("application/json");
+            _client.BaseAddress = new Uri(PostApi);
+            _client.DefaultRequestHeaders.Accept.Add(contentType);
+            _config = config;
+            _tokenService = tokenService;
+        }
+        public async Task<ActionResult> Index()
+        {
+            var response = await _client.GetAsync(PostApi);
+            var strData = await response.Content.ReadAsStringAsync();
+            var options = new JsonSerializerOptions
+            {
+                PropertyNameCaseInsensitive = true
+            };
+            var postDtos = JsonSerializer.Deserialize<List<TagDTO>>(strData, options);
+            return View(postDtos);
+        }
+
+
+    }
+}
